9e463bdfbf9c01ddff2fc28d58e7f4e65aea1251
Andreas Jurat <andreasjurat@Andreass-MacBook-Pro.local>
2016-02-10 15:49:08 +0100

    adding start.js as a module zu parese URL requests

---
+++ start.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+
+function start(){
+	
+	console.log("Startet.");
+	function onRequest(request,response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("Anforderung für Pfad " + pathname + " erhalten.");
+		response.writeHead(200,{"Content-Type": "text/plain"});
+		response.write("Der erste Server!");
+		response.end();
+	}
+	var port= process.env.port || 1337;
+	http.createServer(onRequest).listen(port);
+	console.log("Ist gestartet.");
+}
+
+exports.start = start;
